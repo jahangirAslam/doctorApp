ae00b167c247e58e3b03439e37f85edc2c831044
jahangirAslam <hafiz.j.aslam@gmail.com>
2022-03-04 18:04:03 +0500

    .dashboard

--- src/pages/AddPatient.js
+++ src/pages/AddPatient.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Grid, MenuItem, TextField, Typography } from '@mui/material'
+import noimg from '../assets/images/noimg.png'
 
 const genders = [
     {
@@ -10,7 +11,7 @@ const genders = [
     },
     {
         label: 'others',
-    },   
+    },
 
 ];
 
@@ -54,7 +55,15 @@ const AddPatient = () => {
     };
 
 
+
+
     const [currency, setCurrency] = React.useState('EUR');
+
+
+    var loadFile = function(event) {
+        var image = document.getElementById('output');
+        image.src = URL.createObjectURL(event.target.files[0]);
+    };
     const handleChange = (event) => {
         setCurrency(event.target.value);
     };
@@ -79,19 +88,19 @@ const AddPatient = () => {
                     justifyContent="center"
                     alignItems="center"
                 >
-                    <Grid item lg={12}  xs={12} className="patientFormHeading">
+                    <Grid item lg={12} xs={12} className="patientFormHeading">
                         <h1>Basic Information</h1>
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label=" FirstName" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label=" LastName" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}   xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="Age" type="number" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField
                             id="outlined-select"
                             select
@@ -104,25 +113,32 @@ const AddPatient = () => {
                                     {option.label}
                                 </MenuItem>
                             ))}
-                        </TextField>                       
-                         </Grid>
-                    <Grid item lg={6}   xs={12} className="formEntries">
+                        </TextField>
+                    </Grid>
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="weight" variant="outlined" />
                     </Grid >
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="Allergy" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="phone" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="Address" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}   xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="Email" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
-                        <input type="file" ></input>
+                    <Grid item lg={6} xs={12} className="formEntries">
+                        {/* <img onClick={selectImg} src={noimg} alt="" id="output" /> */}
+                        <div><input type="file" accept="image/*" name="image" id="file" onClick={loadFile()} style="display: none;"> </input></div>
+                        <div><label for="file" style="cursor: pointer;">Upload Image</label></div>
+                        <div><img id="output" width="200" /></div>
+
+
+
+
                     </Grid>
                 </Grid>
                 <Grid item lg={12} xs={12}
@@ -133,19 +149,19 @@ const AddPatient = () => {
                     alignItems="center"
                     style={{ marginTop: "15px" }}
                 >
-                    <Grid item lg={12}  xs={12} className="patientFormHeading">
+                    <Grid item lg={12} xs={12} className="patientFormHeading">
                         <h1>Medical Information</h1>
                     </Grid>
-                    <Grid item lg={6}   xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label=" Height" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label=" Weight" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
-                    <TextField id="outlined-basic" label=" Blood Pressure" variant="outlined" />
+                    <Grid item lg={6} xs={12} className="formEntries">
+                        <TextField id="outlined-basic" label=" Blood Pressure" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField
                             id="outlined-select-currency"
                             select
@@ -159,31 +175,31 @@ const AddPatient = () => {
                                 </MenuItem>
                             ))}
                         </TextField>                        </Grid>
-                    <Grid item lg={6}   xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="Pulse" variant="outlined" />
                     </Grid >
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="Respiration" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="Diet" variant="outlined" />
                     </Grid>
-                    <Grid item lg={6}  xs={12} className="formEntries">
+                    <Grid item lg={6} xs={12} className="formEntries">
                         <TextField id="outlined-basic" label="Allergy" variant="outlined" />
                     </Grid>
                     <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                    item xs={12} 
-                    className="Submitbtn"
-                      > 
-                    <Button>Add Patient</Button> 
+                        container
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                        item xs={12}
+                        className="Submitbtn"
+                    >
+                        <Button>Add Patient</Button>
                     </Grid>
 
                 </Grid>
-                
+
             </Grid>
         </div>
     )
