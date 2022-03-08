fa2464edcee6b82b881479003b7296ddd26ef8df
jahangirAslam <hafiz.j.aslam@gmail.com>
2022-03-08 16:50:19 +0500

    changes

--- src/configs/sidebarNav.js
+++ src/configs/sidebarNav.js
@@ -5,18 +5,8 @@ const sidebarNav = [
         icon: <i className='bx bx-home-alt'></i>,
         text: 'Home'
     },
-    {
-        link: '/patient',
-        section: 'patient',
-        icon: <i className='bx bx-receipt' ></i>,
-        text: 'Add Patient'
-    },
-    {
-        link: '/doctor',
-        section: 'doctor',
-        icon: <i className='bx bx-cube'></i>,
-        text: 'Add Doctor'
-    },
+ 
+ 
     {
         link: '/type',
         section: 'type',
@@ -29,12 +19,7 @@ const sidebarNav = [
         icon: <i className='bx bx-line-chart'></i>,
         text: 'Patient Entry'
     },
-    {
-        link: '/Doctors',
-        section: 'Doctors',
-        icon: <i className='bx bx-cog'></i>,
-        text: 'Doctors'
-    },
+ 
 
   
 ]
